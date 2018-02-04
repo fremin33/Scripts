e05bc014b44028d6a0069f1a9ce2fa61e8c9e189
fremin33 <florian.dev33@gmail.com>
2018-02-04 18:32:36 +0100

    add gitignore

--- githubscript.sh
+++ githubscript.sh
@@ -4,6 +4,7 @@ curl -u 'fremin33' https://api.github.com/user/repos -d "{\"name\":\"${FOLDER}\"
 git init;
 git remote add origin git@github.com:fremin33/${FOLDER}.git;
 echo "# Readme.md" >> README.md;
+echo "node_modules/\nbower_components/" >> .gitignore;
 git add .;
 git commit -m "Initial commit";
 git push -u origin master;
