1e0f8af60eac05d237b61e62e193c2c6e27ba974
fremin33 <florian.dev33@gmail.com>
2018-02-04 18:10:53 +0100

    Initial commit

---
+++ githubscript.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+FOLDER=${PWD##*/}
+curl -u 'fremin33' https://api.github.com/user/repos -d "{\"name\":\"${FOLDER}\"}";
+git init;
+git remote add origin git@github.com:fremin33/${FOLDER}.git;
+echo "# Readme.md" >> README.md;
+git add .;
+git commit -m "Initial commit";
+git push -u origin master;
