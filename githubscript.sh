3d5b3cbdd8646724775329d635d48863fe741c1a
fremin33 <florian.dev33@gmail.com>
2018-02-04 18:53:07 +0100

    add gitignore and readme if don't exist

--- githubscript.sh
+++ githubscript.sh
@@ -1,10 +1,19 @@
 #!bin/bash
-FOLDER=${PWD##*/}
+FOLDER=${PWD##*/};
+README="README.md";
+GITIGNORE=".gitignore";
 curl -u 'fremin33' https://api.github.com/user/repos -d "{\"name\":\"${FOLDER}\"}";
 git init;
 git remote add origin git@github.com:fremin33/${FOLDER}.git;
-echo "# Readme.md" >> README.md;
-echo "node_modules/\nbower_components/" >> .gitignore;
+
+if [ ! -e "${README}" ]; then
+ echo "# Readme.md" >> README.md;
+fi
+
+if [ ! -e "${GITIGNORE}" ]; then
+ echo "node_modules/\nbower_components/" >> .gitignore;
+fi
+
 git add .;
 git commit -m "Initial commit";
 git push -u origin master;
